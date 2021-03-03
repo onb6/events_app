610e2ae2ca38438b0a9ae18a1c647ee2033941cb
onb6 <blierolivia@gmail.com>
2021-03-03 15:58:19 -0500

    config files and scripts

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=5678
+
+CFGD=$(readlink -f ~/.config/events)
+
+if [ ! -e "$CFGD/base" ]; then
+    echo "run deploy first"
+    exit 1
+fi
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+_build/prod/rel/events_app/bin/events_app start
